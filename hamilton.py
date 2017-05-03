946cba04fa7bafa97584d37ebdc2c0f82b2a64f5
Krzysztof Sawarzyński <Krzysztof Sawarzyski>
2017-05-04 00:43:48 +0200

    I hyc o podłogę

--- hamilton.py
+++ hamilton.py
@@ -4,10 +4,10 @@ def find_cycle(graph, v, cycle):
     cycle.append(v)
 
     incidentals = graph_tools.get_incidentals(graph, v)
-    incidentals = filter(lambda x : x not in cycle, incidentals)
+    incidentals = filter(lambda x: x not in cycle, incidentals)
 
     for w in incidentals:
         find_cycle(graph, w, cycle)
-    
-    if len(cycle) != len(graph) or graph_tools.is_incidental(graph, cycle[0], cycle[-1]) == False:
+
+    if len(cycle) != len(graph) or not graph_tools.is_incidental(graph, cycle[0], cycle[-1]):
         cycle.remove(v)
